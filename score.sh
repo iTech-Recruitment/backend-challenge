3ef828a5dbf3411163633c1b432c2073e41fe88a
Lukasz Barcikowski <lukasz@itech.media>
2020-09-29 14:40:28 +0200

    sh separation

--- score.sh
+++ score.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # prep the env
-sudo apt update > /dev/null 2>&1 &
-sudo apt install -y php7.0-curl 2>&1 &
-composer install > /dev/null 2>&1 &
+./install.sh
 
 API_URL="http://18.130.116.85/recipes"
 LOCAL_URL="http://127.0.0.1:8080"
