e53e3538fddfa5377b118d1eb6cbf123550d01bd
Lukasz Barcikowski <lukasz@itech.media>
2020-09-29 14:44:48 +0200

    sh revert, hr config

--- score.sh
+++ score.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # prep the env
-./install.sh
+sudo apt update > /dev/null 2>&1 &
+sudo apt install -y php7.0-curl 2>&1 &
+composer install > /dev/null 2>&1 &
 
 API_URL="http://18.130.116.85/recipes"
 LOCAL_URL="http://127.0.0.1:8080"
